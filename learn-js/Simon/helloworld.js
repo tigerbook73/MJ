6f7fd6dbfd570785ef0666a1bce59b32b5ebbae0
Simon Song <simonsong0816@gmail.com>
2024-03-26 17:35:31 +0800

    Fix helloworld.js

--- learn-js/Simon/helloworld.js
+++ learn-js/Simon/helloworld.js
@@ -1,6 +1,6 @@
 console.log('Hello-0, World!');
 console.log('Hello-1, World!');
- nmmmmmmmmmmmmconsole.log('Hello-2, World!');
+console.log('Hello-2, World!');
 console.log('Hello-3, World!');
 console.log('Hello-4, World!');
 console.log('Hello-5, World!');
