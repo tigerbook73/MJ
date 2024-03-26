a8a1011b2c0972abcaf0bc86781f858f28bbff87
Justin Liao <liaoshuyu0.0@gmail.com>
2024-03-26 20:29:14 +1100

    add hello world

---
+++ learn-js/Justin/helloworld.js
@@ -0,0 +1,8 @@
+console.log('Hello1, World!');
+console.log('Hello2, World!');
+console.log('Hello3, World!');
+console.log('Hello4, World!');
+console.log('Hello5, World!');
+console.log('Hello6, World!');
+console.log('Hello7, World!');
+console.log('Hello8, World!');
