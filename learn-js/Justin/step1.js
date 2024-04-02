8b1045b7fd589671a4f009070f7396ed2e99d852
Justin Liao <liaoshuyu0.0@gmail.com>
2024-04-02 21:51:54 +1100

    create new card obj, add print function

--- learn-js/Justin/step1.js
+++ learn-js/Justin/step1.js
@@ -12,23 +12,94 @@
  */
 
 class CardType {
-  CardTypeSetup(number, type) {
-    (this.cardNumber = number), (this.cardType = type);
+  constructor(type, number, name) {
+    this.type = type;
+    this.number = number;
+    this.name = name;
+  }
+
+  print() {
+    process.stdout.write(this.name);
   }
 }
 
-//const p1 = new cardType(1,"p")
-//const p1 = new cardType(2,"m")
-//const p1 = new cardType(3,"s")
-//const p1 = new cardType(1,"f")
+//const p1 = new CardType(1, 'p');
+//const m2 = new cardType(2,"m")
+//const s3 = new cardType(3,"s")
+//const f1 = new cardType(1,"z")
+
+class CardTypes {
+  constructor() {
+    this.types = [
+      new CardType('m', 1, '一万'),
+      new CardType('m', 2, '二万'),
+      new CardType('m', 3, '三万'),
+      new CardType('m', 4, '四万'),
+      new CardType('m', 5, '五万'),
+      new CardType('m', 6, '六万'),
+      new CardType('m', 7, '七万'),
+      new CardType('m', 8, '八万'),
+      new CardType('m', 9, '九万'),
+      new CardType('p', 1, '一筒'),
+      new CardType('p', 2, '二筒'),
+      new CardType('p', 3, '三筒'),
+      new CardType('p', 4, '四筒'),
+      new CardType('p', 5, '五筒'),
+      new CardType('p', 6, '六筒'),
+      new CardType('p', 7, '七筒'),
+      new CardType('p', 8, '八筒'),
+      new CardType('p', 9, '九筒'),
+      new CardType('s', 1, '一条'),
+      new CardType('s', 2, '二条'),
+      new CardType('s', 3, '三条'),
+      new CardType('s', 4, '四条'),
+      new CardType('s', 5, '五条'),
+      new CardType('s', 6, '六条'),
+      new CardType('s', 7, '七条'),
+      new CardType('s', 8, '八条'),
+      new CardType('s', 9, '九条'),
+      new CardType('z', 1, '东'),
+      new CardType('z', 2, '南'),
+      new CardType('z', 3, '西'),
+      new CardType('z', 4, '北'),
+      new CardType('z', 5, '中'),
+      new CardType('z', 6, '白'),
+      new CardType('z', 7, '发'),
+    ];
+  }
+
+  print() {
+    /*
+    this.types[0].print();
+    for (let x of this.types.slice(1)) {
+      process.stdout.write(',');
+      x.print();
+    }
+    */
+
+    for (let i = 0; i < this.types.length - 1; i++) {
+      this.types[i].print();
+      if (this.types[i].type != this.types[i + 1].type) {
+        console.log();
+        continue;
+      }
+      process.stdout.write(',');
+    }
+    process.stdout.write(this.types[this.types.length - 1].name);
+
+    console.log();
+  }
+}
 
 /*
  * 代表一副牌中一张牌
  */
 class CardTile {
   // 自行定义
-  TileSetup(held, withdrawl, dora) {
-    (this.held = held), (this.withdrawl = withdrawl), (this.dora = dora);
+  constructor(held, withdrawl, dora) {
+    this.held = held;
+    this.withdrawl = withdrawl;
+    this.dora = dora;
   }
 }
 
@@ -66,3 +137,6 @@ mj.init();
 mj.print();
 mj.shuffle();
 mj.print();
+
+const testrun = new CardTypes();
+testrun.print();
