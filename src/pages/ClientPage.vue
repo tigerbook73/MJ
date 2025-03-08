d936878f7fe69bc9a3b1ba5376488c5c7dc18b60
Shengtian Liao <shengtian.l@fcl.software>
2025-03-08 12:58:26 +1100

    Refactor ClientPage.vue to enhance message handling and UI layout for client interactions

--- src/pages/ClientPage.vue
+++ src/pages/ClientPage.vue
@@ -4,92 +4,140 @@
       <q-btn to="/">Home</q-btn>
 
       <div class="q-my-md text-h6">{{ connected ? "Connected" : "Disconnected" }}</div>
-      <div class="row q-gutter-sm">
-        <q-btn dense @click="signIn">Sign In</q-btn>
-        <q-btn dense @click="signOut">Sign Out</q-btn>
+      <div class="column q-gutter-md">
+        <!-- list client -->
+        <div class="row justify-between">
+          <q-space />
+          <q-btn dense @click="listClient">List Client</q-btn>
+        </div>
+
+        <!-- sign in -->
+        <div class="row justify-between">
+          <q-input v-model="email" label="Email" dense outlined />
+          <q-input v-model="password" label="Password" dense outlined />
+          <q-btn dense @click="signIn">Sign In</q-btn>
+        </div>
+
+        <!-- sign out -->
+        <div class="row justify-between">
+          <q-space />
+          <q-btn dense @click="signOut">Sign Out</q-btn>
+        </div>
+
+        <!-- list room -->
+        <div class="row justify-between">
+          <q-space />
+          <q-btn dense @click="listRoom">List Room</q-btn>
+        </div>
+
+        <!-- join room -->
+        <div class="row justify-between">
+          <q-input v-model="roomName" label="Room Name" dense outlined />
+          <q-select
+            v-model="position"
+            label="Position"
+            class="col"
+            dense
+            outlined
+            :options="[Position.East, Position.South, Position.West, Position.North]"
+          />
+          <q-btn dense @click="joinRoom">Join Room</q-btn>
+        </div>
       </div>
     </div>
+
     <q-separator vertical />
+
     <div class="col column">
       <!-- request -->
       <q-item>
         <q-item-section>Request</q-item-section>
-        <q-btn @click="requestList = []" dense>Clean</q-btn>
+        <q-btn @click="messageList = []" dense>Clean</q-btn>
       </q-item>
       <q-scroll-area class="col">
         <q-list bordered>
           <q-expansion-item
-            v-for="request in requestList"
-            :key="request.time"
+            v-for="message in messageList"
+            :key="message.label"
             expand-separator
-            :label="request.time + ' > ' + request.request.type"
+            :label="message.label"
             dense
           >
-            <q-input
-              type="textarea"
-              :model-value="JSON.stringify(request.request, null, 2)"
-              autogrow
-              dense
-              readonly
-              class="q-ml-lg"
-            ></q-input>
+            <div v-if="message.request" class="q-pl-lg">
+              <div>=></div>
+              <q-input type="textarea" :model-value="message.request" autogrow dense readonly></q-input>
+            </div>
+            <div v-if="message.response" class="q-pl-lg">
+              <div>&lt;=</div>
+              <q-input type="textarea" :model-value="message.response" autogrow dense readonly></q-input>
+            </div>
           </q-expansion-item>
         </q-list>
       </q-scroll-area>
 
       <q-separator></q-separator>
-
-      <!-- response or event -->
-      <q-item>
-        <q-item-section>Response</q-item-section>
-        <q-btn @click="responseList = []" dense>Clean</q-btn>
-      </q-item>
-      <q-scroll-area class="col">
-        <q-list bordered>
-          <q-expansion-item
-            v-for="response in responseList"
-            :key="response.time"
-            expand-separator
-            :label="response.time + ' > ' + response.response.type"
-            dense
-          >
-            <q-input
-              type="textarea"
-              :model-value="JSON.stringify(response.response, null, 2)"
-              autogrow
-              readonly
-              dense
-              class="q-ml-lg"
-            ></q-input>
-          </q-expansion-item>
-        </q-list>
-      </q-scroll-area>
     </div>
   </q-page>
 </template>
 
 <script lang="ts">
-interface RequestRecord {
-  time: string;
-  request: {
-    type: GameRequestType;
-    data?: unknown;
-  };
-}
+class MessageRecord {
+  type: string;
+  label: string = "";
+  request_time: string | null;
+  request: any | null;
+  response_time: string | null;
+  response: any | null;
 
-interface ResponseRecord {
-  time: string;
-  response: {
-    type: GameRequestType;
-    data?: unknown;
-  };
+  constructor(
+    type: string,
+    request_time: string | null,
+    request: any,
+    response_time: string | null,
+    response: any | null,
+  ) {
+    this.type = type;
+    this.request_time = request_time;
+    this.request = request;
+    this.response_time = response_time;
+    this.response = response;
+  }
+
+  getTimeLabel() {
+    return this.request_time || this.response_time || "N/A";
+  }
+
+  getTypeLabel() {
+    return this.type || "N/A";
+  }
+
+  getLabel() {
+    let label = this.getTimeLabel() + " - " + this.getTypeLabel();
+    if (this.request && !this.response) {
+      label += "...";
+    }
+    return label;
+  }
+
+  refreshLabel() {
+    this.label = this.getLabel();
+  }
 }
 </script>
 
 <script setup lang="ts">
 import dayjs from "dayjs";
 import { clientApi } from "src/client/client-api";
-import { GameRequestType, GameResponse } from "src/common/protocols/apis.models";
+import { Position } from "src/common/core/mj.game";
+import {
+  GameRequestType,
+  GameResponse,
+  JoinRoomRequest,
+  ListClientRequest,
+  ListRoomRequest,
+  SignInRequest,
+  SignOutRequest,
+} from "src/common/protocols/apis.models";
 import { ref } from "vue";
 
 defineOptions({
@@ -104,19 +152,36 @@ clientApi.gameSocket.onDisconnect(() => {
   connected.value = false;
 });
 
-const requestList = ref([] as RequestRecord[]);
-const responseList = ref([] as ResponseRecord[]);
+const messageList = ref([] as MessageRecord[]);
 
-function updateRequest(type: GameRequestType, data: unknown): void {
-  requestList.value.unshift({ time: dayjs().format("YYYY-MM-DD HH:mm:ss SSS"), request: { type, data } });
+function appendMessage(type: string, request: any | null, response: any | null): MessageRecord {
+  const message = new MessageRecord(
+    type,
+    request ? dayjs().format("YYYY-MM-DD HH:mm:ss SSS") : null,
+    request ? JSON.stringify(request, null, 2) : null,
+    response ? dayjs().format("YYYY-MM-DD HH:mm:ss SSS") : null,
+    response ? JSON.stringify(response, null, 2) : null,
+  );
+  message.refreshLabel();
+  messageList.value.unshift(message);
+  return message;
 }
 
-function updateResponse(type: GameRequestType, data: unknown): void {
-  responseList.value.unshift({ time: dayjs().format("YYYY-MM-DD HH:mm:ss SSS"), response: { type, data } });
+function updateMessage(message: MessageRecord, request: any | null, response: any | null): void {
+  if (request) {
+    message.request = request ? JSON.stringify(request, null, 2) : null;
+    message.request_time = dayjs().format("YYYY-MM-DD HH:mm:ss SSS");
+  }
+  if (response) {
+    message.response = response ? JSON.stringify(response, null, 2) : null;
+    message.response_time = dayjs().format("YYYY-MM-DD HH:mm:ss SSS");
+  }
+  message.refreshLabel();
+  messageList.value = [...messageList.value];
 }
 
-clientApi.gameSocket.onReceive((data: GameResponse) => {
-  updateResponse(data.type, data.data);
+clientApi.gameSocket.onReceive((response: GameResponse) => {
+  appendMessage(response.type, null, response.data);
 });
 
 /**
@@ -124,20 +189,81 @@ clientApi.gameSocket.onReceive((data: GameResponse) => {
  */
 const email = ref("player1@gmail.com");
 const password = ref("123456");
-
 async function signIn() {
-  const request = {
-    email: email.value,
-    password: password.value,
+  const request: SignInRequest = {
+    type: GameRequestType.SIGN_IN,
+    data: {
+      email: email.value,
+      password: password.value,
+    },
   };
-  updateRequest(GameRequestType.SIGN_IN, request);
-  const data = await clientApi.signIn(request.email, request.password);
-  updateResponse(GameRequestType.SIGN_IN, data);
+  const message = appendMessage(request.type, request.data, null);
+  try {
+    const data = await clientApi.signIn(request.data.email, request.data.password);
+    updateMessage(message, null, data);
+  } catch (error: any) {
+    updateMessage(message, null, { message: error.message });
+  }
 }
 
 async function signOut() {
-  updateRequest(GameRequestType.SIGN_OUT, {});
-  const data = await clientApi.signOut();
-  updateResponse(GameRequestType.SIGN_OUT, data);
+  const request: SignOutRequest = {
+    type: GameRequestType.SIGN_OUT,
+    data: {},
+  };
+  const message = appendMessage(request.type, request.data, null);
+  try {
+    const data = await clientApi.signOut();
+    updateMessage(message, null, {});
+  } catch (error: any) {
+    updateMessage(message, null, { message: error.message });
+  }
+}
+
+async function listClient() {
+  const request: ListClientRequest = {
+    type: GameRequestType.LIST_CLIENT,
+    data: {},
+  };
+  const message = appendMessage(request.type, request.data, null);
+  try {
+    const data = await clientApi.listClient();
+    updateMessage(message, null, data);
+  } catch (error: any) {
+    updateMessage(message, null, { message: error.message });
+  }
+}
+
+async function listRoom() {
+  const request: ListRoomRequest = {
+    type: GameRequestType.LIST_ROOM,
+    data: {},
+  };
+  const message = appendMessage(request.type, request.data, null);
+  try {
+    const data = await clientApi.listRoom();
+    updateMessage(message, null, data);
+  } catch (error: any) {
+    updateMessage(message, null, { message: error.message });
+  }
+}
+
+const roomName = ref("default");
+const position = ref(Position.North);
+async function joinRoom() {
+  const request: JoinRoomRequest = {
+    type: GameRequestType.JOIN_ROOM,
+    data: {
+      roomName: roomName.value,
+      position: position.value,
+    },
+  };
+  const message = appendMessage(request.type, request.data, null);
+  try {
+    const data = await clientApi.joinRoom(request.data.roomName, request.data.position);
+    updateMessage(message, null, data);
+  } catch (error: any) {
+    updateMessage(message, null, { message: error.message });
+  }
 }
 </script>
