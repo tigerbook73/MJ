577ac282995a21d1609e89c6792cd4a7984032e5
Shengtian Liao <tigerbook73@outlook.com>
2025-04-29 22:36:25 +1000

    Add GameRoom component and update RoomPage to display game rooms

---
+++ src/example/components/GameRoom.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="fit flex column bg-blue-3 text-center">
+    <!-- top -->
+    <div class="row col-3">
+      <q-space />
+      <div class="col-6 column flex-center bg-yellow-3">
+        <div>北</div>
+        <div>north player</div>
+      </div>
+      <q-space />
+    </div>
+
+    <!-- middle -->
+    <div class="row col-6">
+      <!-- left -->
+      <div class="col-3 column flex-center bg-yellow-3">
+        <div>西</div>
+        <div>west player</div>
+      </div>
+      <div class="col-6 row flex-center bg-green-3">{{ props.room.name }}</div>
+      <div class="col-3 column flex-center bg-yellow-3">
+        <div>東</div>
+        <div>east player</div>
+      </div>
+    </div>
+
+    <!-- bottom -->
+    <div class="row col-3">
+      <q-space />
+      <div class="col-6 column flex-center bg-yellow-3">
+        <div>南</div>
+        <div>south player</div>
+      </div>
+      <q-space />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export interface GameRoomProp {
+  id: number;
+  name: string;
+}
+</script>
+
+<script setup lang="ts">
+import { defineProps } from "vue";
+
+const props = defineProps<{
+  room: GameRoomProp;
+}>();
+</script>
--- src/example/pages/RoomPage.vue
+++ src/example/pages/RoomPage.vue
@@ -1,11 +1,28 @@
 <template>
-  <q-page class="row flex-center">
-    <q-card style="width: 50%">
-      <q-card-section>
-        <div>This is rome Page</div>
+  <q-page class="row justify-center">
+    <q-card class="col-8">
+      <q-card-section class="row flex-center q-col-gutter-md">
+        <div v-for="room in rooms" :key="room.id" class="col-3 room">
+          <GameRoom :room="room" />
+        </div>
       </q-card-section>
     </q-card>
   </q-page>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import GameRoom, { GameRoomProp } from "src/example/components/GameRoom.vue";
+import { ref } from "vue";
+
+const rooms = ref<GameRoomProp[]>([
+  { id: 1, name: "Room 1" },
+  { id: 2, name: "Room 2" },
+  { id: 3, name: "Room 3" },
+]);
+</script>
+
+<style scoped lang="scss">
+.room {
+  aspect-ratio: 1 / 1;
+}
+</style>
