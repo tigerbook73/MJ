971747ac4068f99973d1f3db05d4aba2140be89d
Shengtian Liao <tigerbook73@outlook.com>
2025-04-29 17:35:25 +1000

    Add example layout and pages with routing configuration

---
+++ src/example/pages/ConnectingPage.vue
@@ -0,0 +1,12 @@
+<template>
+  <q-page class="row flex-center">
+    <q-card style="width: 50%">
+      <q-card-section>
+        <div class="text-center">Connecting</div>
+      </q-card-section>
+      <q-inner-loading color="primary" :showing="true"> </q-inner-loading>
+    </q-card>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
---
+++ src/example/pages/SignInPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <q-page class="row flex-center">
+    <q-card style="width: 50%">
+      <q-card-section>
+        <div>This is sign-in Page</div>
+      </q-card-section>
+    </q-card>
+  </q-page>
+</template>
+
+<script setup lang="ts"></script>
