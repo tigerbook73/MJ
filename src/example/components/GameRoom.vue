4db0294dc52a909861eb7848305f946fb74198c6
Shengtian Liao <tigerbook73@outlook.com>
2025-04-29 23:05:00 +1000

    Add GamePlayer component and integrate it into GameRoom and RoomPage for player display

--- src/example/components/GameRoom.vue
+++ src/example/components/GameRoom.vue
@@ -3,34 +3,30 @@
     <!-- top -->
     <div class="row col-3">
       <q-space />
-      <div class="col-6 column flex-center bg-yellow-3">
-        <div>北</div>
-        <div>north player</div>
-      </div>
+      <GamePlayer
+        :player="props.room.players.north"
+        class="col-6"
+        @click="handlePlayerClick(props.room.players.north)"
+      />
       <q-space />
     </div>
 
     <!-- middle -->
     <div class="row col-6">
       <!-- left -->
-      <div class="col-3 column flex-center bg-yellow-3">
-        <div>西</div>
-        <div>west player</div>
-      </div>
+      <GamePlayer :player="props.room.players.west" class="col-3" @click="handlePlayerClick(props.room.players.west)" />
       <div class="col-6 row flex-center bg-green-3">{{ props.room.name }}</div>
-      <div class="col-3 column flex-center bg-yellow-3">
-        <div>東</div>
-        <div>east player</div>
-      </div>
+      <GamePlayer :player="props.room.players.east" class="col-3" @click="handlePlayerClick(props.room.players.east)" />
     </div>
 
     <!-- bottom -->
     <div class="row col-3">
       <q-space />
-      <div class="col-6 column flex-center bg-yellow-3">
-        <div>南</div>
-        <div>south player</div>
-      </div>
+      <GamePlayer
+        :player="props.room.players.south"
+        class="col-6"
+        @click="handlePlayerClick(props.room.players.south)"
+      />
       <q-space />
     </div>
   </div>
@@ -40,13 +36,24 @@
 export interface GameRoomProp {
   id: number;
   name: string;
+  players: {
+    east: GamePlayerProp;
+    west: GamePlayerProp;
+    north: GamePlayerProp;
+    south: GamePlayerProp;
+  };
 }
 </script>
 
 <script setup lang="ts">
 import { defineProps } from "vue";
+import GamePlayer, { GamePlayerProp } from "./GamePlayer.vue";
 
 const props = defineProps<{
   room: GameRoomProp;
 }>();
+
+function handlePlayerClick(player: GamePlayerProp) {
+  console.log(`Player [${player.name}] at position [${player.position}] in room [${props.room.name}] clicked`);
+}
 </script>
--- src/example/pages/RoomPage.vue
+++ src/example/pages/RoomPage.vue
@@ -2,7 +2,7 @@
   <q-page class="row justify-center">
     <q-card class="col-8">
       <q-card-section class="row flex-center q-col-gutter-md">
-        <div v-for="room in rooms" :key="room.id" class="col-3 room">
+        <div v-for="room in rooms" :key="room.id" class="col-6 col-md-4 col-xl-2 room">
           <GameRoom :room="room" />
         </div>
       </q-card-section>
@@ -11,13 +11,41 @@
 </template>
 
 <script setup lang="ts">
+import { Position } from "src/common/core/mj.game";
 import GameRoom, { GameRoomProp } from "src/example/components/GameRoom.vue";
 import { ref } from "vue";
 
 const rooms = ref<GameRoomProp[]>([
-  { id: 1, name: "Room 1" },
-  { id: 2, name: "Room 2" },
-  { id: 3, name: "Room 3" },
+  {
+    id: 1,
+    name: "Room 1",
+    players: {
+      east: { id: 1, position: Position.East, name: "Player East", email: "" },
+      south: { id: 4, position: Position.South, name: "Player South", email: "" },
+      west: { id: 2, position: Position.West, name: "Player West", email: "" },
+      north: { id: 3, position: Position.North, name: "Player North", email: "" },
+    },
+  },
+  {
+    id: 2,
+    name: "Room 2",
+    players: {
+      east: { id: 1, position: Position.East, name: "Player East", email: "" },
+      south: { id: 4, position: Position.South, name: "Player South", email: "" },
+      west: { id: 2, position: Position.West, name: "Player West", email: "" },
+      north: { id: 3, position: Position.North, name: "Player North", email: "" },
+    },
+  },
+  {
+    id: 3,
+    name: "Room 3",
+    players: {
+      east: { id: 1, position: Position.East, name: "Player East", email: "" },
+      south: { id: 4, position: Position.South, name: "Player South", email: "" },
+      west: { id: 2, position: Position.West, name: "Player West", email: "" },
+      north: { id: 3, position: Position.North, name: "Player North", email: "" },
+    },
+  },
 ]);
 </script>
 
