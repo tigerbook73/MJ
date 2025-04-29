a2c809cc682803be667f746b768287dee4bb7831
Shengtian Liao <tigerbook73@outlook.com>
2025-04-29 23:10:19 +1000

    Update RoomPage layout and player data for improved display and accuracy

--- src/example/pages/RoomPage.vue
+++ src/example/pages/RoomPage.vue
@@ -2,7 +2,7 @@
   <q-page class="row justify-center">
     <q-card class="col-8">
       <q-card-section class="row flex-center q-col-gutter-md">
-        <div v-for="room in rooms" :key="room.id" class="col-6 col-md-4 col-xl-2 room">
+        <div v-for="room in rooms" :key="room.id" class="col-12 col-md-6 col-xl-4 room">
           <GameRoom :room="room" />
         </div>
       </q-card-section>
@@ -20,30 +20,30 @@ const rooms = ref<GameRoomProp[]>([
     id: 1,
     name: "Room 1",
     players: {
-      east: { id: 1, position: Position.East, name: "Player East", email: "" },
-      south: { id: 4, position: Position.South, name: "Player South", email: "" },
-      west: { id: 2, position: Position.West, name: "Player West", email: "" },
-      north: { id: 3, position: Position.North, name: "Player North", email: "" },
+      east: { id: 1, position: Position.East, name: "Player East", email: "east@email.com" },
+      south: { id: 2, position: Position.South, name: "Player South", email: "" },
+      west: { id: 3, position: Position.West, name: "Player West", email: "" },
+      north: { id: 4, position: Position.North, name: "Player North", email: "" },
     },
   },
   {
     id: 2,
     name: "Room 2",
     players: {
-      east: { id: 1, position: Position.East, name: "Player East", email: "" },
-      south: { id: 4, position: Position.South, name: "Player South", email: "" },
-      west: { id: 2, position: Position.West, name: "Player West", email: "" },
-      north: { id: 3, position: Position.North, name: "Player North", email: "" },
+      east: { id: 5, position: Position.East, name: "Player East", email: "" },
+      south: { id: 6, position: Position.South, name: "Player South", email: "" },
+      west: { id: 7, position: Position.West, name: "Player West", email: "" },
+      north: { id: 8, position: Position.North, name: "Player North", email: "" },
     },
   },
   {
     id: 3,
     name: "Room 3",
     players: {
-      east: { id: 1, position: Position.East, name: "Player East", email: "" },
-      south: { id: 4, position: Position.South, name: "Player South", email: "" },
-      west: { id: 2, position: Position.West, name: "Player West", email: "" },
-      north: { id: 3, position: Position.North, name: "Player North", email: "" },
+      east: { id: 9, position: Position.East, name: "Player East", email: "" },
+      south: { id: 10, position: Position.South, name: "Player South", email: "" },
+      west: { id: 11, position: Position.West, name: "Player West", email: "" },
+      north: { id: 12, position: Position.North, name: "Player North", email: "" },
     },
   },
 ]);
