9303266d9a5154916d241d61692b521cec49ce4f
Justin Liao <liaoshuyu0.0@gmail.com>
2025-05-02 22:21:52 +1000

    fix indentation

--- src/components/BoardAreaB.vue
+++ src/components/BoardAreaB.vue
@@ -1,24 +1,17 @@
 <template>
-<<<<<<< HEAD
   <div class="column flex-center area-board">
     <div class="row flex-center">
       <!-- <comp-tile :type="userMj.right_display.name" :selected="false" position="right" size="large"></comp-tile> -->
     </div>
   </div>
-=======
-  <div class="area-board">{{ $options.name }}</div>
->>>>>>> origin/main
 </template>
 
 <script setup lang="ts">
 defineOptions({
   name: "BoardAreaB",
 });
-<<<<<<< HEAD
 
 // import CompTile from "components/CompTile.vue";
 // import { useMjStore } from "src/stores/mj-store";
 // const userMj = useMjStore();
-=======
->>>>>>> origin/main
 </script>
--- src/components/BoardAreaC.vue
+++ src/components/BoardAreaC.vue
@@ -1,13 +1,9 @@
 <template>
-  <<<<<<< HEAD
   <div class="column flex-center area-board">
     <div class="row flex-center">
       <!-- <comp-tile :type="userMj.left_display.name" :selected="false" position="left" size="large"></comp-tile> -->
     </div>
   </div>
-  =======
-  <div class="area-board">{{ $options.name }}</div>
-  >>>>>>> origin/main
 </template>
 
 <script setup lang="ts">
