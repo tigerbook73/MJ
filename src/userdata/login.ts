12bbd0c4047940f92d910518cdb81a2aae5552a2
Shengtian Liao <tigerbook73@outlook.com>
2025-05-03 12:06:51 +1000

    remove useless files

--- src/userdata/login.ts
+++
@@ -1,6 +0,0 @@
-// import { GameResponse } from "src/common/protocols/apis.models";
-// import { onSocketReceive } from "src/websocket/socket";
-
-// onSocketReceive((data: GameResponse) => {
-//   console.log(data);
-// });
