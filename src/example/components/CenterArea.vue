8e1a05d30414b75e1567f196b81fc4411a40afc2
Shengtian Liao <tigerbook73@outlook.com>
2025-05-04 19:41:13 +1000

    fix: update chip rotation in CenterArea component

--- src/example/components/CenterArea.vue
+++ src/example/components/CenterArea.vue
@@ -3,20 +3,20 @@
     <!-- top -->
     <div class="col-3 row flex-center">
       <q-space />
-      <q-chip>上</q-chip>
+      <q-chip><div class="rotate-180">上</div></q-chip>
       <q-space />
     </div>
 
     <!-- center -->
     <div class="col-6 row flex-center">
       <div class="col-3 row flex-center">
-        <q-chip>左</q-chip>
+        <q-chip><div class="rotate-90">左</div></q-chip>
       </div>
       <div class="col row flex-center">
         <q-btn color="dark">start / pause / reset</q-btn>
       </div>
       <div class="col-3 row flex-center">
-        <q-chip>右</q-chip>
+        <q-chip><div class="rotate-270">右</div></q-chip>
       </div>
     </div>
 
