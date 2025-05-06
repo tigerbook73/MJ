f7adc3cfc5e9a1153596f5b235ff44a67645c2bc
Shengtian Liao <tigerbook73@outlook.com>
2025-05-06 16:48:47 +1000

    feat: enhance PlayerAreaMe component with improved tile display and action buttons

--- src/example/components/PlayerAreaMe.vue
+++ src/example/components/PlayerAreaMe.vue
@@ -1,19 +1,31 @@
 <template>
   <div class="row flex-center">
-    <div class="wx-4"></div>
-    <div class="wx-16 row flex-center">
-      <game-tile
-        v-for="tile in tiles"
-        :key="tile.id"
-        :tile="tile"
-        @click.stop="handleClick(tile.id)"
-        @dblclick.stop="handleDropClick(tile.id)"
-      ></game-tile>
+    <div class="wx-20 row flex-center">
+      <div class="col">
+        <div v-for="set in openTiles" :key="set[0].id" class="row items-center q-gutter-xs">
+          <div class="row items-center">
+            <game-tile v-for="tile in set" :key="tile.id" :tile="tile" :size="size"></game-tile>
+          </div>
+        </div>
+      </div>
+
+      <div class="row justify-end items-center">
+        <game-tile
+          v-for="tile in handTiles"
+          :key="tile.id"
+          :tile="tile"
+          @click.stop="handleClick(tile.id)"
+          @dblclick.stop="dropTile(tile.id)"
+        ></game-tile>
+      </div>
     </div>
-    <div class="wx-4 row flex-center">
-      <q-btn v-if="showDrop" @click="handleDrop" dense label="DROP" color="dark" />
-      <q-btn v-if="showHu" @click="handleHu" dense label="Hu" color="dark" />
-      <q-btn v-if="showPass" @click="handlePass" dense label="Pass" color="dark" />
+    <div class="wx-4 row flex-center q-gutter-xs">
+      <q-btn v-if="showDrop.show" :disable="showDrop.disabled" @click="handleDrop" dense label="出牌" color="dark" />
+      <q-btn v-if="showZimo.show" :disable="showZimo.disabled" @click="handleZimo" dense label="自摸" color="dark" />
+      <q-btn v-if="showPass.show" :disable="showPass.disabled" @click="handlePass" dense label="过" color="dark" />
+      <q-btn v-if="showGang.show" :disable="showGang.disabled" @click="handleGang" dense label="杠" color="dark" />
+      <q-btn v-if="showPeng.show" :disable="showPeng.disabled" @click="handlePeng" dense label="碰" color="dark" />
+      <q-btn v-if="showChi.show" :disable="showChi.disabled" @click="handleChi" dense label="吃" color="dark" />
     </div>
   </div>
 </template>
@@ -23,6 +35,15 @@ enum State {
   None = 0,
   MyTurn = 1,
   WaitingPass = 2,
+}
+
+interface ShowState {
+  show: boolean;
+  disabled: boolean;
+}
+
+function canDo(showState: ShowState) {
+  return showState.show && !showState.disabled;
 }
 </script>
 
@@ -41,7 +62,7 @@ const exampleStore = useExampleStore();
 
 const size = "md";
 
-const tiles = computed<GameTileProp[]>(() => {
+const handTiles = computed<GameTileProp[]>(() => {
   const position = CommonUtil.mapPosition(exampleStore.currentPosition!, Direction.Bottom);
   const player = exampleStore.currentGame!.players[position]!;
   const tileIds = player.handTiles.slice();
@@ -56,6 +77,23 @@ const tiles = computed<GameTileProp[]>(() => {
       selected: selectedTiles.value.includes(id),
     }),
   );
+});
+
+const openTiles = computed<GameTileProp[][]>(() => {
+  const position = CommonUtil.mapPosition(exampleStore.currentPosition!, Direction.Bottom);
+  const player = exampleStore.currentGame!.players[position]!;
+  const tiless = player.openedSets.map((set): GameTileProp[] =>
+    set.tiles.map((tile): GameTileProp => {
+      return {
+        id: tile,
+        direction: Direction.Bottom,
+        size,
+        back: false,
+        selected: selectedTiles.value.includes(tile),
+      };
+    }),
+  );
+  return tiless;
 });
 
 // game state
@@ -73,7 +111,7 @@ const state = computed(() => {
 
 // selected
 const selectedTiles = ref<TileId[]>([]);
-function selectToDrop(tileId: TileId) {
+function setSelected(tileId: TileId) {
   if (tileId === TileCore.voidId) {
     return;
   }
@@ -86,29 +124,61 @@ function selectToDrop(tileId: TileId) {
   }
 }
 
+function addSelected(tileId: TileId) {
+  if (tileId === TileCore.voidId) {
+    return;
+  }
+
+  const index = selectedTiles.value.indexOf(tileId);
+  if (index === -1) {
+    selectedTiles.value.push(tileId);
+  } else {
+    selectedTiles.value.splice(index, 1);
+  }
+}
+
+function clearSelected() {
+  selectedTiles.value = [];
+}
+
 function handleClick(tileId: TileId) {
+  if (tileId === TileCore.voidId) {
+    return;
+  }
+
   // in state my turn
   if (state.value === State.MyTurn) {
-    selectToDrop(tileId);
+    setSelected(tileId);
     return;
   }
 
   // in state waitiing pass, chi / peng / hu / gang is not supported now
   if (state.value === State.WaitingPass) {
+    addSelected(tileId);
     return;
   }
 }
 
+//
 // drop tile feature
-const showDrop = computed(() => state.value === State.MyTurn && selectedTiles.value.length > 0);
-function handleDropClick(tileId: TileId) {
+//
+const showDrop = computed<ShowState>(() => {
+  return {
+    show: state.value === State.MyTurn,
+    disabled: selectedTiles.value.length === 0,
+  };
+});
+
+// double click to drop
+function dropTile(tileId: TileId) {
   if (tileId === TileCore.voidId) {
     return;
   }
 
-  if (state.value === State.MyTurn) {
+  if (canDo(showDrop.value)) {
     try {
       clientApi.actionDrop(tileId); // Use the tileId parameter instead of selectedTiles.value[0]
+      clearSelected();
     } catch (e) {
       $q.notify({
         message: "Drop failed",
@@ -121,31 +191,33 @@ function handleDropClick(tileId: TileId) {
 
 // drop button
 function handleDrop() {
-  handleDropClick(selectedTiles.value[0]);
+  dropTile(selectedTiles.value[0]);
 }
 
-// hu feature
-const showHu = computed(() => {
+//
+// zimo feature
+//
+const showZimo = computed<ShowState>(() => {
   if (state.value === State.MyTurn) {
-    const game = exampleStore.currentGame!;
-    const player = exampleStore.currentGame!.current!;
-    const tiles = player.handTiles.slice();
-    if (player.picked !== TileCore.voidId) {
-      tiles.push(player.picked);
-    }
-    return game.canHu(tiles);
+    const player = exampleStore.currentGame!.players[exampleStore.currentPosition!]!;
+    return {
+      show: TileCore.canHu(player.handTiles, player.picked),
+      disabled: false,
+    };
   }
-  return false;
+  return {
+    show: false,
+    disabled: false,
+  };
 });
 
-function handleHu() {
-  const game = exampleStore.currentGame!;
-  if (showHu.value) {
+function handleZimo() {
+  if (canDo(showZimo.value)) {
     try {
-      clientApi.actionHu();
+      clientApi.actionZimo();
     } catch (e) {
       $q.notify({
-        message: "Hu failed",
+        message: "Zimo failed",
         color: "negative",
         icon: "warning",
       });
@@ -153,16 +225,143 @@ function handleHu() {
   }
 }
 
+//
 // pass feature
-const showPass = computed(() => state.value === State.WaitingPass);
+//
+const showPass = computed(() => {
+  return {
+    show: state.value === State.WaitingPass,
+    disabled: false,
+  };
+});
 function handlePass() {
-  const game = exampleStore.currentGame!;
   if (showPass.value) {
     try {
       clientApi.actionPass();
     } catch (e) {
       $q.notify({
         message: "Pass failed",
+        color: "negative",
+        icon: "warning",
+      });
+    }
+  }
+}
+
+//
+// peng feature
+//
+const showPeng = computed<ShowState>(() => {
+  if (state.value !== State.WaitingPass) {
+    return {
+      show: false,
+      disabled: false,
+    };
+  }
+
+  const player = exampleStore.currentGame!.players[exampleStore.currentPosition!]!;
+  const latestTile = exampleStore.currentGame!.latestTile!;
+  return {
+    show: TileCore.canPeng(player.handTiles, latestTile),
+    disabled:
+      selectedTiles.value.length !== 2 || !TileCore.isSame(selectedTiles.value[0], selectedTiles.value[1], latestTile),
+  };
+});
+
+function handlePeng() {
+  const game = exampleStore.currentGame!;
+  if (canDo(showPeng.value)) {
+    try {
+      clientApi.actionPeng([selectedTiles.value[0], selectedTiles.value[1]]);
+    } catch (e) {
+      $q.notify({
+        message: "Peng failed",
+        color: "negative",
+        icon: "warning",
+      });
+    }
+  }
+}
+
+//
+// gang feature
+//
+const showGang = computed<ShowState>(() => {
+  if (state.value !== State.WaitingPass) {
+    return {
+      show: false,
+      disabled: false,
+    };
+  }
+
+  const player = exampleStore.currentGame!.players[exampleStore.currentPosition!]!;
+  const latestTile = exampleStore.currentGame!.latestTile!;
+  return {
+    show: TileCore.canGang(player.handTiles, latestTile),
+    disabled:
+      selectedTiles.value.length !== 3 ||
+      !TileCore.isSame(selectedTiles.value[0], selectedTiles.value[1], selectedTiles.value[2], latestTile),
+  };
+});
+
+function handleGang() {
+  const game = exampleStore.currentGame!;
+  if (canDo(showGang.value)) {
+    try {
+      clientApi.actionGang([selectedTiles.value[0], selectedTiles.value[1], selectedTiles.value[2]]);
+    } catch (e) {
+      $q.notify({
+        message: "Gang failed",
+        color: "negative",
+        icon: "warning",
+      });
+    }
+  }
+}
+
+//
+// chi feature
+//
+const showChi = computed<ShowState>(() => {
+  if (state.value !== State.WaitingPass) {
+    return {
+      show: false,
+      disabled: false,
+    };
+  }
+
+  const player = exampleStore.currentGame!.players[exampleStore.currentPosition!]!;
+  const currentPlayer = exampleStore.currentGame!.current!;
+  // find the next player
+  let nextPosition = (currentPlayer.position - 1 + 4) % 4;
+  while (!exampleStore.currentGame!.players[nextPosition]) {
+    nextPosition = (nextPosition - 1 + 4) % 4;
+  }
+  // player is the next player
+  if (player.position !== nextPosition) {
+    return {
+      show: false,
+      disabled: false,
+    };
+  }
+
+  const latestTile = exampleStore.currentGame!.latestTile!;
+  return {
+    show: TileCore.canChi(player.handTiles, latestTile),
+    disabled:
+      selectedTiles.value.length !== 2 ||
+      !TileCore.isConsecutive(selectedTiles.value[0], selectedTiles.value[1], latestTile),
+  };
+});
+
+function handleChi() {
+  const game = exampleStore.currentGame!;
+  if (canDo(showChi.value)) {
+    try {
+      clientApi.actionChi([selectedTiles.value[0], selectedTiles.value[1]]);
+    } catch (e) {
+      $q.notify({
+        message: "Chi failed",
         color: "negative",
         icon: "warning",
       });
