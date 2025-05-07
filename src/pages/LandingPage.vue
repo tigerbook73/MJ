29500a2ed272ebe26fae2d218b1a471e02f0fb65
Shengtian Liao <tigerbook73@outlook.com>
2025-05-07 15:55:35 +1000

    feat: add LandingPage component and update root route to use it

---
+++ src/pages/LandingPage.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="fullscreen row flex-center q-gutter-md">
+    <q-btn href="/#/example" label="Example Site" size="lg" class="col-2" icon="person" />
+    <q-btn href="/#/simon" label="Simon Site" size="lg" class="col-2" icon="person" />
+    <q-btn href="/#/justin" label="Justin Site" size="lg" class="col-2" icon="person" />
+  </div>
+</template>
+
+<script setup lang="ts">
+defineOptions({
+  name: "ErrorNotFound",
+});
+</script>
