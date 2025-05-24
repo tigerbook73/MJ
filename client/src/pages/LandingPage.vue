3b764c36c2ec09705d2870aa0e4994edb74a7e88
Shengtian Liao <tigerbook73@outlook.com>
2025-05-25 00:27:09 +1000

    update vueRouterMode to history and refactor LandingPage navigation buttons

--- client/src/pages/LandingPage.vue
+++ client/src/pages/LandingPage.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="fullscreen row flex-center q-gutter-md">
-    <q-btn href="/#/example" label="Example Site" size="lg" class="col-2" icon="person" />
-    <q-btn href="/#/simon" label="Simon Site" size="lg" class="col-2" icon="person" />
-    <q-btn href="/#/justin" label="Justin Site" size="lg" class="col-2" icon="person" />
+    <q-btn label="Example Site" size="lg" class="col-2" icon="person" @click="() => navigateTo('/example')" />
+    <q-btn label="Simon Site" size="lg" class="col-2" icon="person" @click="() => navigateTo('/simon')" />
+    <q-btn label="Justin Site" size="lg" class="col-2" icon="person" @click="() => navigateTo('/justin')" />
   </div>
 </template>
 
 <script setup lang="ts">
 defineOptions({
-  name: "ErrorNotFound",
+  name: "LandingPage",
 });
+
+function navigateTo(path: string) {
+  window.location.href = path;
+}
 </script>
