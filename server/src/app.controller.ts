c14b21ee9823d65cbb46a2622918095957f3545f
Liao Shengtian <liaoshengtian@hotmail.com>
2025-05-24 23:47:30 +1000

    update server configuration and add static file serving (#37)

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -5,12 +5,12 @@ import { AppService } from "./app.service";
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  @Get("/hello")
   getHello(): string {
     return this.appService.getHello();
   }
 
-  @Get("test")
+  @Get("/test")
   getTest(): string {
     return "test";
   }
