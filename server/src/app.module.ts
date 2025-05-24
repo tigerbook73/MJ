b694e60f506abdb90492ff2e649fa50ecb7ddbba
Shengtian Liao <tigerbook73@outlook.com>
2025-05-24 23:46:51 +1000

    update server configuration and add static file serving

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -3,10 +3,16 @@ import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { MjGameModule } from "./game/mj-game.module";
 import { ScheduleModule } from "@nestjs/schedule";
+import { join } from "path";
+import { ServeStaticModule } from "@nestjs/serve-static";
 
 @Module({
   imports: [
-    //
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, "..", "..", "client", "dist", "spa"), // Path to your built SPA's dist/build directory
+      serveRoot: "/", // Serve static files from the root URL
+      renderPath: "*", // Fallback for all unmatched paths to index.html
+    }),
     MjGameModule,
     ScheduleModule.forRoot(),
   ],
