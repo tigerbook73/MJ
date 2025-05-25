634ae400ac0d7ae8bd01b041b288368f9b3b07aa
Shengtian Liao <tigerbook73@outlook.com>
2025-05-25 12:44:20 +1000

    fix: spa fallback

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,9 +1,27 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
+import * as Express from "express";
+import { join } from "path";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
+
+  const spaPath = ["..", "..", "client", "dist", "spa"];
+  app.use(Express.static(join(__dirname, ...spaPath)));
+  app.use(
+    (
+      req: Express.Request,
+      res: Express.Response,
+      next: Express.NextFunction,
+    ) => {
+      if (req.method === "GET" && !req.url.startsWith("/api")) {
+        res.sendFile(join(__dirname, ...spaPath, "index.html"));
+      } else {
+        next();
+      }
+    },
+  );
   await app.listen(3000);
 }
 bootstrap();
