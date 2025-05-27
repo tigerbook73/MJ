375c1570c03a5d286c5b5b1b003153ae7d618fb6
Shengtian Liao <tigerbook73@outlook.com>
2025-05-27 20:16:33 +1000

    feat: add PrismaService and its test suite

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,6 +4,7 @@ import { AppService } from "./app.service";
 import { MjGameModule } from "./game/mj-game.module";
 import { ScheduleModule } from "@nestjs/schedule";
 import { ConfigModule } from "@nestjs/config";
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [
@@ -13,6 +14,6 @@ import { ConfigModule } from "@nestjs/config";
     ScheduleModule.forRoot(),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
