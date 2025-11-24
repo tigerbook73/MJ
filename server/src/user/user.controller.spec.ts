853183fb934d0ae25038fe26c730070d82f800d4
Liao Shengtian <liaoshengtian@hotmail.com>
2025-11-24 15:05:37 +1100

    Fix: import paths in test files to use correct module aliases. (#87)

--- server/src/user/user.controller.spec.ts
+++ server/src/user/user.controller.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { UserController } from "./user.controller";
+import { UserService } from "src/game/user.service";
 
 describe("UserController", () => {
   let controller: UserController;
@@ -7,6 +8,14 @@ describe("UserController", () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UserController],
+      providers: [
+        {
+          provide: UserService,
+          useValue: {
+            list: jest.fn().mockResolvedValue([]),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<UserController>(UserController);
