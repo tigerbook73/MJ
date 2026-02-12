014e84fd89c422afce80e8ece42f6ea8ba010406
Liao Shengtian <liaoshengtian@hotmail.com>
2026-02-12 23:32:31 +1100

    feat(api): add API definitions and operations for user authentication and management (#106)

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,31 +1,17 @@
 import "dotenv/config";
 import { NestFactory } from "@nestjs/core";
 import { ValidationPipe } from "@nestjs/common";
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
 import { AppModule } from "./app.module";
 import * as Express from "express";
 import { join } from "path";
+import { createSwaggerDocument } from "./app.open-api";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.setGlobalPrefix("api");
   app.useGlobalPipes(new ValidationPipe());
 
-  // Swagger configuration
-  const config = new DocumentBuilder()
-    .setTitle("MJ Game API")
-    .setDescription("Mahjong Game REST API Documentation")
-    .setVersion("1.0")
-    .build();
-
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup("api/docs", app, document, {
-    swaggerOptions: {
-      tagsSorter: "alpha",
-      schemasSorter: "alpha", // currently not working, TODO
-      persistAuthorization: true,
-    },
-  });
+  createSwaggerDocument(app);
 
   // using process.cwd(), command must start from server's root directory
   const publicPath = join(process.cwd(), "public");
